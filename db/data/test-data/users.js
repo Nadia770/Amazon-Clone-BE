53831f6fae0cd4b7de5628ce7baf9fa8b0c1d8a4
Nadia <nadia.m.amir@gmail.com>
2021-06-03 11:07:14 +0100

    create users data

---
+++ db/data/test-data/users.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    username: 'AT',
+    name: 'Alex Tristram',
+    avatar_url: 'https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg',
+    vote_count: 0
+  },
+  {
+    username: 'NA',
+    name: 'Nadia Amir',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/24604688?s=460&v=4',
+    vote_count: 0,
+  },
+  {
+    username: 'Sarah',
+    name: 'Sarah Jane',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/24394918?s=400&v=4',
+    vote_count: 0
+  }
+];
