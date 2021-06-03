bb1ce0b3c0e3223381c1b1abef24e47b65949340
Alex Tristram <allex.tristram.gmail.com>
2021-06-03 11:58:19 +0100

    add master index and node env to test data file

---
+++ db/index.js
@@ -0,0 +1,8 @@
+const ENV = process.env.NODE_ENV || "development";
+
+// const devData = require("./development-data");
+const testData = require("./data/test-data");
+
+const data = { test: testData, development: devData, production: devData };
+
+module.exports = data[ENV];
