717f88ad1f113dea3d5878cf27c8fd8e7936654f
Alex Tristram <allex.tristram.gmail.com>
2021-06-03 11:42:26 +0100

    add fix to review.js test data

--- db/data/test-data/reviews.js
+++ db/data/test-data/reviews.js
@@ -1,21 +1,23 @@
-module.exports= [{
-  body: "Great product, happy with my purchase",
-  created_at: 1511354163389,
-  author: "AT",
-  helpful_count: 5,
-  star_count: 5,
-}]
-module.exports= [{
-  body: "Item looked like it had been used before, requested a refund",
-  created_at: 1479818163389,
-  author: "Sarah",
-  helpful_count: 2,
-  star_count: 2,
-}]
-module.exports= [{
-  body: "Regular shmegular",
-  created_at: 11416746163389,
-  author: "AT",
-  helpful_count: 5,
-  star_count: 2,
-}]
+module.exports = [
+  {
+    body: "Great product, happy with my purchase",
+    created_at: 1511354163389,
+    author: "AT",
+    helpful_count: 5,
+    star_count: 5,
+  },
+  {
+    body: "Item looked like it had been used before, requested a refund",
+    created_at: 1479818163389,
+    author: "Sarah",
+    helpful_count: 2,
+    star_count: 2,
+  },
+  {
+    body: "Regular shmegular",
+    created_at: 11416746163389,
+    author: "AT",
+    helpful_count: 5,
+    star_count: 2,
+  },
+];
