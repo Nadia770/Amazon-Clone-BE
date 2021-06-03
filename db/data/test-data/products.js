bd7ccc50ebbdb490531b3b4787233870ffa2d11f
Nadia <nadia.m.amir@gmail.com>
2021-06-03 11:05:53 +0100

    create test data for products

---
+++ db/data/test-data/products.js
@@ -0,0 +1,25 @@
+module.exports = [{
+  name: 'Whiz Blender',
+  price: 300,
+  image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.ikohs.com%2Fuk%2F910422%2Fblender-matt-800w-american-style-jug-blender.jpg&imgrefurl=https%3A%2F%2Fwww.ikohs.com%2Fuk%2Fbuy-glass-blenders%2F70674-blender-matt-800w-american-style-jug-blender.html&tbnid=1F_R-zhUWlBmoM&vet=12ahUKEwjiuprEiPvwAhWOwYUKHfwwDUEQMygAegUIARDYAg..i&docid=UgxOQma2QSs60M&w=1500&h=1500&q=blender&ved=2ahUKEwjiuprEiPvwAhWOwYUKHfwwDUEQMygAegUIARDYAg",
+  average_rating: 0,
+  description: "Personal Blender, Sboly Smoothie Blender Single Serve Small Blender for Juice Shakes and Smoothie, Smoothie Maker with 20 oz Tritan BPA-Free Blender Cup, UK Plug",
+  in_stock: true
+},
+{
+  name: 'Teddys Toaster',
+  price: 100,
+  image: "https://www.amazon.co.uk/Russell-Hobbs-2-Slice-Toaster-18780/dp/B008HQ63LO?source=ps-sl-shoppingads-lpcontext&psc=1?source=ps-sl-shoppingads-lpcontext&psc=1%27",
+  average_rating: 0,
+  description: "nspiring premium plastic high-gloss textured design complemented by sleek chrome Accents with soft illumination on buttons while in use",
+  in_stock: true
+},
+{
+  name: 'Russell Hobbs 24360 Inspire Electric Kettle,',
+  price: 300,
+  image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.ikohs.com%2Fuk%2F910422%2Fblender-matt-800w-american-style-jug-blender.jpg&imgrefurl=https%3A%2F%2Fwww.ikohs.com%2Fuk%2Fbuy-glass-blenders%2F70674-blender-matt-800w-american-style-jug-blender.html&tbnid=1F_R-zhUWlBmoM&vet=12ahUKEwjiuprEiPvwAhWOwYUKHfwwDUEQMygAegUIARDYAg..i&docid=UgxOQma2QSs60M&w=1500&h=1500&q=blender&ved=2ahUKEwjiuprEiPvwAhWOwYUKHfwwDUEQMygAegUIARDYAg",
+  average_rating: 0,
+  description: "Personal Blender, Sboly Smoothie Blender Single Serve Small Blender for Juice Shakes and Smoothie, Smoothie Maker with 20 oz Tritan BPA-Free Blender Cup, UK Plug",
+  in_stock: true
+}
+]
