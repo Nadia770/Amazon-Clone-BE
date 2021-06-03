c0600675c93db809355d0bcc575fe4540a06849b
Nadia <nadia.m.amir@gmail.com>
2021-06-03 11:31:31 +0100

    add columns to review test data

--- db/data/test-data/reviews.js
+++ db/data/test-data/reviews.js
@@ -3,5 +3,19 @@ module.exports= [{
   created_at: 1511354163389,
   author: "AT",
   helpful_count: 5,
+  star_count: 5,
+}]
+module.exports= [{
+  body: "Item looked like it had been used before, requested a refund",
+  created_at: 1479818163389,
+  author: "Sarah",
+  helpful_count: 2,
+  star_count: 2,
+}]
+module.exports= [{
+  body: "Regular shmegular",
+  created_at: 11416746163389,
+  author: "AT",
+  helpful_count: 5,
   star_count: 2,
 }]
