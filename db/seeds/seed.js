1ca7983f5e1a38f974ad168a4471cb3c2a70b254
Nadia <nadia.m.amir@gmail.com>
2021-06-07 14:52:09 +0100

    seed product test data

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,1 +1,19 @@
-const { productData, reviewData, userData } = require("../data/index.js");
+const { productData, reviewData, userData } = require("../data/test-data");
+const { changeProductsKey } = require("../utils/utils")
+
+
+exports.seed = function (knex) {
+  const formattedProductData = changeProductsKey(productData,
+    "product_name",
+    "name",
+    "product_url",
+    "image",
+    "product_description",
+    "description")
+  return knex
+    .insert(formattedProductData)
+    .into('products')
+    .then(()=>{
+      return knex
+    })
+}
