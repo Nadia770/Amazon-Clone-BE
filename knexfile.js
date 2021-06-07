ae43a98780787662ebcd452da0273644665ad309
Nadia <nadia.m.amir@gmail.com>
2021-06-07 12:30:50 +0100

    create config object for test/dev enviornment

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,16 @@
 const ENV = process.env.NODE_ENV || "development";
 const { DB_URL } = process.env;
 
-const dbConfig = {
-  client: "pg",
-  connection: { database: "amazon_clone_test" },
-  seed: { directory: "./db/seeds" },
-};
+const dbConfig =
+  development = {
+    client: "pg",
+    connection: { database: "amazon_clone" },
+    seed: { directory: "./db/seeds" },
+  },
+  test = {
+    client: "pg",
+    connection: { database: "amazon_clone_test" },
+    seed: { directory: "./db/seeds" },
+  };
+
+module.exports = dbConfig[ENV]
