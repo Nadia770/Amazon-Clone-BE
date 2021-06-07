2a95e4d30623dd75efd24f09a7075e29ac9f469b
Alex Tristram <allex.tristram.gmail.com>
2021-06-07 20:46:06 +0100

    add makeRefObject function with testing

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,4 +1,4 @@
-const { changeProductsKey, addProductID } = require("../db/utils/utils");
+const { changeProductsKey, makeRefObject } = require("../db/utils/utils");
 
 describe("changeProductsKey", () => {
   test("should return an empty array if passed an empty array", () => {
@@ -184,43 +184,75 @@ describe("changeProductsKey", () => {
   });
 });
 
-
-describe.only("addProductID", () => {
+describe.only("makeRefObject", () => {
   test("should return an empty array if passed an empty array", () => {
-    expect(addProductID([])).toEqual([]);
-  })
-  test("should add a key of product_id to review data object", () => {
-    const reviewData =
-      [{
-        body: "Great product, happy with my purchase",
-        created_at: 1511354163389,
-        author: "AT",
-        helpful_count: 5,
-        star_count: 5,
-        product: "Whiz Blender"
-      }]
-
-    const productData = [
+    expect(makeRefObject([])).toEqual({});
+  });
+  test("should return a new reference object", () => {
+    let productData = [
       {
         product_id: 1,
-        product_name: 'Whiz Blender',
+        product_name: "Whiz Blender",
         price: 300,
         average_rating: 0,
-        product_description: 'Personal Blender, Sboly Smoothie Blender Single Serve Small Blender for Juice Shakes and Smoothie, Smoothie Maker with 20 oz Tritan BPA-Free Blender Cup, UK Plug',
-        product_url: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.ikohs.com%2Fuk%2F910422%2Fblender-matt-800w-american-style-jug-blender.jpg&imgrefurl=https%3A%2F%2Fwww.ikohs.com%2Fuk%2Fbuy-glass-blenders%2F70674-blender-matt-800w-american-style-jug-blender.html&tbnid=1F_R-zhUWlBmoM&vet=12ahUKEwjiuprEiPvwAhWOwYUKHfwwDUEQMygAegUIARDYAg..i&docid=UgxOQma2QSs60M&w=1500&h=1500&q=blender&ved=2ahUKEwjiuprEiPvwAhWOwYUKHfwwDUEQMygAegUIARDYAg',
-        in_stock: true
-      }
-    ]
+        product_description:
+          "Personal Blender, Sboly Smoothie Blender Single Serve Small Blender for Juice Shakes and Smoothie, Smoothie Maker with 20 oz Tritan BPA-Free Blender Cup, UK Plug",
+        product_url:
+          "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.ikohs.com%2Fuk%2F910422%2Fblender-matt-800w-american-style-jug-blender.",
+        in_stock: true,
+      },
+    ];
 
-    const formattedReviewDataObj = [{
-      body: "Great product, happy with my purchase",
-      created_at: 1511354163389,
-      author: "AT",
-      helpful_count: 5,
-      star_count: 5,
-      product: "Whiz Blender",
-      product_id: 1
-    }]
-    expect(addProductID(productData, reviewData, "product_id")).toEqual(formattedReviewDataObj)
-  })
-})
+    let actual = { 1: "Whiz Blender" };
+
+    expect(makeRefObject(productData, "product_id", "product_name")).toEqual(
+      actual
+    );
+  });
+  test("should return a new reference object from an multiple objects", () => {
+    let productData = [
+      {
+        product_name: "Whiz Blender",
+        price: 300,
+        image:
+          "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.ikohs.com%2Fuk%2F910422%2Fblender-matt-800w-american-style-jug-blender.jpg&imgrefurl=https%3A%2F%2Fwww.ikohs.com%2Fuk%2Fbuy-glass-blenders%2F70674-blender-matt-800w-american-style-jug-blender.html&tbnid=1F_R-zhUWlBmoM&vet=12ahUKEwjiuprEiPvwAhWOwYUKHfwwDUEQMygAegUIARDYAg..i&docid=UgxOQma2QSs60M&w=1500&h=1500&q=blender&ved=2ahUKEwjiuprEiPvwAhWOwYUKHfwwDUEQMygAegUIARDYAg",
+        average_rating: 0,
+        description:
+          "Personal Blender, Sboly Smoothie Blender Single Serve Small Blender for Juice Shakes and Smoothie, Smoothie Maker with 20 oz Tritan BPA-Free Blender Cup, UK Plug",
+        in_stock: true,
+        product_id: 1,
+      },
+      {
+        product_name: "Teddys Toaster",
+        price: 100,
+        image:
+          "https://www.amazon.co.uk/Russell-Hobbs-2-Slice-Toaster-18780/dp/B008HQ63LO?source=ps-sl-shoppingads-lpcontext&psc=1?source=ps-sl-shoppingads-lpcontext&psc=1%27",
+        average_rating: 0,
+        description:
+          "nspiring premium plastic high-gloss textured design complemented by sleek chrome Accents with soft illumination on buttons while in use",
+        in_stock: true,
+        product_id: 2,
+      },
+      {
+        product_name: "Russell Hobbs 24360 Inspire Electric Kettle",
+        price: 300,
+        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.ikohs.",
+        average_rating: 0,
+        description:
+          "Personal Blender, Sboly Smoothie Blender Single Serve Small Blender for Juice Shakes and Smoothie, Smoothie Maker with 20 oz Tritan BPA-Free Blender Cup, UK Plug",
+        in_stock: true,
+        product_id: 3,
+      },
+    ];
+
+    let actual = {
+      1: "Whiz Blender",
+      2: "Teddys Toaster",
+      3: "Russell Hobbs 24360 Inspire Electric Kettle",
+    };
+
+    expect(makeRefObject(productData, "product_id", "product_name")).toEqual(
+      actual
+    );
+  });
+});
