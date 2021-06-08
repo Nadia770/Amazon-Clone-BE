4c64ad3acb1c85389fbd299a05bfeb1d986ed788
Alex Tristram <allex.tristram@gmail.com>
2021-06-08 13:50:04 +0100

    start of express route app

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
-apiRouter = require("./Router/apiRouter")
+apiRouter = require("./Router/apiRouter");
+const dbConnection = require("./db/dbConnection");
 
+beforeEach(() => dbConnection.seed.run());
+afterAll(() => dbConnection.destroy());
 
-module.exports = app
+describe("Name of the group", () => {
+  test("should ", () => {});
+});
+
+module.exports = app;
