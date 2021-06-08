2d3da629e040f833bb3862b58c212f05126b07c5
Alex Tristram <allex.tristram.gmail.com>
2021-06-08 11:24:15 +0100

    add email and location test data

--- db/data/test-data/users.js
+++ db/data/test-data/users.js
@@ -1,21 +1,28 @@
 module.exports = [
   {
-    username: 'AT',
-    name: 'Alex Tristram',
-    avatar_url: 'https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg',
-    vote_count: 0
+    username: "AT",
+    name: "Alex Tristram",
+    avatar_url:
+      "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+    vote_count: 0,
+    email: "hello@gmail.com",
+    location: "USA",
   },
   {
-    username: 'NA',
-    name: 'Nadia Amir',
-    avatar_url: 'https://avatars2.githubusercontent.com/u/24604688?s=460&v=4',
+    username: "NA",
+    name: "Nadia Amir",
+    avatar_url: "https://avatars2.githubusercontent.com/u/24604688?s=460&v=4",
     vote_count: 0,
+    location: "Somalia",
+
+    email: "helloNadia@gmail.com",
   },
   {
-    username: 'Sarah',
-    name: 'Sarah Jane',
-    avatar_url: 'https://avatars2.githubusercontent.com/u/24394918?s=400&v=4',
+    username: "Sarah",
+    name: "Sarah Jane",
+    avatar_url: "https://avatars2.githubusercontent.com/u/24394918?s=400&v=4",
     vote_count: 0,
-    location: "United Kingdom"
-  }
+    location: "United Kingdom",
+    email: "helloAlex@gmail.com",
+  },
 ];
